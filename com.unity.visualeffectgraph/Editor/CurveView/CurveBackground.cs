65281910e4b0e3ff690e1ea20633fed5d82d81b1
Tristan Genevet <tristan@unity3d.com>
2018-10-17 19:50:34 +0200

    Fixed horizontal lines.

--- com.unity.visualeffectgraph/Editor/CurveView/CurveBackground.cs
+++ com.unity.visualeffectgraph/Editor/CurveView/CurveBackground.cs
@@ -26,19 +26,71 @@ namespace UnityEditor.VFX.CurveView
             }
         }
 
-        Mesh m_HorizontalLinesMesh;
+        Mesh m_HoriAxis;
 
         public CurveBackground()
         {
-            m_HorizontalLinesMesh = new Mesh();
+            CreateHoriAxis();
+        }
 
-            m_HorizontalLinesMesh.vertices = new Vector3[] { new Vector3(0, 0.5f, 0), new Vector3(1, 0.5f, 0) };
-            m_HorizontalLinesMesh.colors32 = new Color32[] { Color.black, Color.black };
-            m_HorizontalLinesMesh.SetIndices(new int[] { 0, 1 }, MeshTopology.Lines, 0);
-            
-            if (s_Mat == null)
+        void CreateHoriAxis()
+        {
+            if(m_HoriAxis != null)
+                return;
+            m_HoriAxis = new Mesh();
+            m_HoriAxis.vertices = new Vector3[] { new Vector3(0, 1, 0), new Vector3(1, 1, 0), new Vector3(1, 0, 0), new Vector3(0, 0, 0) };
+            m_HoriAxis.colors32 = new Color32[] { Color.black, Color.black, Color.black, Color.black };
+            m_HoriAxis.SetIndices(new int[] { 0, 1, 2, 3 }, MeshTopology.Quads, 0);
+        }
+
+        Mesh m_HorizontalLines;
+
+        const int minSpace = 30;
+        const int maxSpace = 100;
+        float m_HorizontalMargin;
+
+        void UpdateHorizontalLines()
+        {
+            if( m_HorizontalLines == null)
             {
-                s_Mat = (Material)EditorGUIUtility.LoadRequired("SceneView/HandleLines.mat");
+                m_HorizontalLines = new Mesh();
+            }
+            float height = contentRect.height;
+            int currentCount = m_HorizontalLines.vertexCount / 4;
+
+            float units = 1;
+
+            while(units * view.scale.y * height > maxSpace)
+            {
+                units /= 5;
+            }
+            while (units * view.scale.y * height < minSpace)
+            {
+                units *= 5;
+            }
+
+            m_HorizontalMargin = units * view.scale.y * height;
+
+            int neededCount = (int)(height / m_HorizontalMargin) + 1;
+
+            if (neededCount != currentCount)
+            {
+
+                m_HorizontalLines.SetIndices(null, MeshTopology.Lines, 0);
+                var vertices = new Vector3[neededCount * 2];
+                var colors = new Color32[neededCount * 2];
+
+                for(int i = 0; i< neededCount; ++i)
+                {
+                    float y = i;
+                    vertices[i * 2] = new Vector3(0,y,0);
+                    vertices[i * 2 + 1] = new Vector3(1, y, 0);
+                    colors[i * 2] = colors[i * 2 + 1] = new Color32(0, 0, 0, 128);
+                }
+
+                m_HorizontalLines.vertices = vertices;
+                m_HorizontalLines.colors32 = colors;
+                m_HorizontalLines.SetIndices(Enumerable.Range(0, neededCount * 2).ToArray(), MeshTopology.Lines, 0);
             }
         }
 
@@ -48,9 +100,17 @@ namespace UnityEditor.VFX.CurveView
             float height = contentRect.height;
             float width = contentRect.width;
 
-            s_Mat.SetPass(0);
+            if (s_Mat == null)
+            {
+                s_Mat = (Material)EditorGUIUtility.LoadRequired("SceneView/HandleLines.mat");
+            }
 
-            Graphics.DrawMeshNow(m_HorizontalLinesMesh, Matrix4x4.TRS(new Vector3(0,view.offset.y,0),Quaternion.identity,new Vector3(width, height, 0)));
+            s_Mat.SetPass(0);
+            CreateHoriAxis();
+            Graphics.DrawMeshNow(m_HoriAxis, Matrix4x4.TRS(new Vector3(0, view.offset.y + height * 0.5f, 0), Quaternion.identity, new Vector3(width, 3, 0)));
+
+            UpdateHorizontalLines();
+            Graphics.DrawMeshNow(m_HorizontalLines, Matrix4x4.TRS(new Vector3(0, Mathf.Repeat(view.offset.y + height * 0.5f, m_HorizontalMargin) , 0), Quaternion.identity, new Vector3(width, m_HorizontalMargin, 1)));
         }
     }
 }
